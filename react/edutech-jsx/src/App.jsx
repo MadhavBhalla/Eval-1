72be93a6c14626beda761380114a771a9647e711
nishant-2005 <nishant2082.be23@chitkara.edu.in>
2024-10-29 13:51:02 +0530

    about react

--- react/edutech-jsx/src/App.jsx
+++ react/edutech-jsx/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Header from './components/header';
-import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure the bundle is imported for dropdowns
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutPage from './webpages/About/AboutPage';
 import './style.css';
 import './App.css';
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <div>
       <Header />
       {/* Main content goes here */}
+      <AboutPage />
       <Footer />
     </div>
   );
